b5d3719acdbf37249a3ce65542928c91aff672e4
Hleb <primetime2pm@gmail.com>
2022-08-08 14:28:08 +0300

    Первая программа

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ('Один из последних просмотреных фильмов', ''),
+    b = +prompt ('На сколько оцените его?', '');
+const c = prompt ('Один из последних просмотреных фильмов', ''),
+    d = +prompt ('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
